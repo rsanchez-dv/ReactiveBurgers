3ff7a12871572701ded5e740959c312015ff8404
Roberto Sanchez <rsanchez92@live.com>
2019-10-03 10:58:40 -0700

    Base Axios project connecting to Firebase

--- burgerapp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerapp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -72,6 +73,24 @@ class BurgerBuilder extends Component {
     }
     purchaseContinueHandler = () =>{
         alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            // Recalculate price on server
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Roberto Sanchez',
+                address:{
+                    street:'123 Fake St.',
+                    zipCode: '90061',
+                    country: 'USA',
+                },
+                email: 'asdf@asdf.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post(`/orders.json`,order)
+        .then(response=> console.log(response))
+        .catch(error => console.log(error));
     }
     render(){
         const disabledInfo = {
