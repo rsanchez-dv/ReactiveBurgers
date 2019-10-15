ed8155f15fb13b15383f600fb2ebf623c5e2a15c
Roberto Sanchez <rsanchez92@live.com>
2019-10-15 12:30:40 -0700

    restructured project to get it to work on netlify

---
+++ build/precache-manifest.756437f0cdf7841bb4ced740104e8957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7022f868ce501652d9c8defe00a1eaf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1820a0ed38b287ef4297",
+    "url": "/static/css/main.377d7c95.chunk.css"
+  },
+  {
+    "revision": "41016aa0782e1414fe46",
+    "url": "/static/js/2.444bb752.chunk.js"
+  },
+  {
+    "revision": "1820a0ed38b287ef4297",
+    "url": "/static/js/main.3cdcbd1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/26.1 burger-logo.png.b8503d26.png"
+  }
+]);
