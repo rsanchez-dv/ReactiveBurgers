f634f5696e65aa49d110cc38dd8ef5d01abda0ee
Roberto Sanchez <rsanchez92@live.com>
2020-02-27 10:51:47 -0800

    Refactored reducer code
    Made it more clean

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,6 @@
 import * as actionTypes from '../actions/actionsTypes';
+import {updateObject} from '../utility';
+
 // Global State
 const initialState = {
     ingredients: null,
@@ -12,50 +14,46 @@ const INGREDIENT_PRICES = {
     meat: 1.3,
     bacon: 0.7
 }
+const addIngredient = (state,action) => {
+    const updatedIngredient = {[action.ingredientName]: state.ingredients[action.ingredientName] + 1}
+    const updatedIngredients = updateObject(state.ingredients,updatedIngredient)
 
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedState);
+}
+const removeIngredient = (state,action) => {
+    const updatedIngredient = {[action.ingredientName]: state.ingredients[action.ingredientName] - 1}
+    const updatedIngredients= updateObject(state.ingredients,updatedIngredient)
+
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+    }
+    return updateObject(state, updatedState);
+}
+const setIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+        },
+        // You could pull this from server but for now its hard coded
+        totalPrice: 4,
+        error: false
+    });
+}
 const reducer = (state = initialState,action) =>{
     // What type of action are we doing?
     switch(action.type){
-        case actionTypes.ADD_INGREDIENT:
-            // Run Add Ingredient and update the global store
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            };
-        case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] -  1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            };
-        case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
-                // The following one line is all you need to set the ingredients to the app however
-                // the order will be from the database not the order that we wanted
-                // ingredients: action.ingredients,
-                ingredients: {
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat
-                },
-                // You could pull this from server but for now its hard coded
-                totalPrice: 4,
-                error: false
-            }
-         case actionTypes.FETCH_INGREDIENTS_FAILED:
-             return {
-                 ...state,
-                 error: true
-             }
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state,action);
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state,action);
+        case actionTypes.SET_INGREDIENTS: return setIngredients(state,action)
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return updateObject(state,{error: true});
         default: return state;
     }
 }
