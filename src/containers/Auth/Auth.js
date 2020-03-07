ddfb4d69a51a46d46379c73bd1ae1b86c13f33f0
Roberto Sanchez <rsanchez92@live.com>
2020-03-06 16:40:03 -0800

    Hooked up Redux to the Auth page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import classes from './Auth.css'
+import * as actions from '../../store/actions/index'
+import {connect} from 'react-redux';
 class Auth extends Component {
     state = {
         controls: {
@@ -63,6 +65,10 @@ class Auth extends Component {
         };
         this.setState({controls: updatedControls})
     }
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    }
     render () {
         const formElementArray = [];
         for (let key in this.state.controls) {
@@ -86,7 +92,7 @@ class Auth extends Component {
         ));
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     <h2>Register</h2>
                     {form}
                     <Button btnType="Success">Submit</Button>
@@ -95,4 +101,10 @@ class Auth extends Component {
         );
     }
 }
-export default Auth; 
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email,password))
+    }
+}
+export default connect(null,mapDispatchToProps)(Auth); 
